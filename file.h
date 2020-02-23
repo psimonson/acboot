c14f1afc602cca9663162d7bc92e5a9b5b3f12d1
Philip Simonson <psimonson1988@gmail.com>
2020-02-22 20:19:39 -0500

    Updated sector count of files.

--- file.h
+++ file.h
@@ -11,13 +11,13 @@
 
 /* some defines for io sys */
 #define IO_START	2		/* starting sector of operating system */
-#define IO_SECTORS	12		/* total operating system sectors */
+#define IO_SECTORS	13		/* total operating system sectors */
 /* some defines for binary app */
 #define SHELL_START (IO_START+IO_SECTORS)+1
 #define SHELL_SECTORS 16
 /* some defines for graph app */
 #define GRAPH_START (SHELL_START+SHELL_SECTORS)+1
-#define GRAPH_SECTORS 10
+#define GRAPH_SECTORS 11
 /* some defines for hello app */
 #define HELLO_START (GRAPH_START+GRAPH_SECTORS)+1
 #define HELLO_SECTORS 9
